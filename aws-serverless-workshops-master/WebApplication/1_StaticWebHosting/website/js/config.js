0c36f05b0eefbc83216a8491ab338ef17dc7e644
Akanksha Lonhari <alonhari@andonline.com>
2020-03-24 14:57:57 -0700

    AWS Web App Test Code

---
+++ aws-serverless-workshops-master/WebApplication/1_StaticWebHosting/website/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_DUzr8jCah', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4h69e18sbs6sjmu5uoltt05ka3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://d853y9md00.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
